fdcffeb6ba19b8d995f3e7b01e68fde4f7d2e28e
s.edinin <baboot.dev@gmail.com>
2017-06-29 09:20:56 +0400

    config fix

--- config/cart.php
+++ config/cart.php
@@ -7,7 +7,7 @@ return [
     'storage' => env('CART_STORAGE')?:'DB',
     'storageDrivers'=>[
         'DB'    => \Baboot\Storage\DB::class,
-        'Redis' => \Baboot\\Storage\Redis::class,
+        'Redis' => \Baboot\Storage\Redis::class,
 
     ]
 ];
